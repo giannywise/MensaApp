b89c189d76c96460d59b5e2891adf55e7bd51efb
giannywise <90458284+giannywise@users.noreply.github.com>
2022-08-30 18:15:07 +0200

    Co-authored-by: RamazanKaymaz <RamazanKaymaz@users.noreply.github.com> Co-authored-by: Michel Hansen <MiHa207@users.noreply.github.com> Co-authored-by: Flip1099 <Flip1099@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { TailwindProvider } from 'tailwindcss-react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
-export default function App() {
+ function App() {
   return (
-    <TailwindProvider>
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </TailwindProvider>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home}/>
+    </Stack.Navigator>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { StyleSheet,View,Text,TouchableOpacity,SafeAreaView } from "react-native";
+
+const Home = props => {
+
+
+
+};
+
+const styles = StyleSheet.create({
+
+
+
+});
+
+export default Home;
